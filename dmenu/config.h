765ce0afed3587ab08056430f21dcb40c9983740
yang-arch <yangxinyu75@live.com>
2024-02-17 10:21:18 +0800

    update color

--- dmenu/config.h
+++ dmenu/config.h
@@ -13,8 +13,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#eeeeee", "#775500" },
+	[SchemeOut] = { "#000000", "#ffff00" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
