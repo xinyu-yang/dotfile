5f6f1bcd6543dd7a8bfc804019904f3a15f38524
yang-arch <yangxinyu75@live.com>
2024-08-05 02:19:28 +0800

    set font to bold

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
-//static char *font = "MesloLGS NF:pixelsize=14:antialias=true:autohint=true";
+//static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
+static char *font = "MesloLGS NF:pixelsize=14:antialias=true:autohint=true:style=Bold";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
