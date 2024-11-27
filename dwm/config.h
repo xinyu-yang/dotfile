d23b8abf53263922a7516685b3cf2f4d0e2bf58f
yang@arch <yangxinyu75@live.com>
2024-11-27 21:29:03 +0800

    update config for lf, ncmpcpp, fcitx5, Xresources, ...

--- dwm/config.h
+++ dwm/config.h
@@ -16,7 +16,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "wmfont:size=7:style=Bold", "WenQuanYi Micro Hei Mono:size=8", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static char *fonts[]          = { "wmfont:size=10:style=Bold", "WenQuanYi Micro Hei Mono:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static char normfgcolor[]           = "#bbbbbb";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
